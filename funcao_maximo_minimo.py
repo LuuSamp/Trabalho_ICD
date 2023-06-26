4319ef7c6456e8de738a4471cd555dc244ac6ef6
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-26 16:51:53 -0300

    Associando uma cor para cada país

--- funcao_maximo_minimo.py
+++ funcao_maximo_minimo.py
@@ -4,31 +4,32 @@ def funcao_maximo_minimo(dataframe, nome_coluna, nome_coluna_pais):
     '''
     Essa função irá receber um DataFrame juntamente com a sua coluna que deve ser 
     analisada. A função tem o objetivo de analisar os dois maiores valores dessa 
-    coluna e os dois menores valores dessa coluna. Associa os valores aos nomes dos
-    países usando a coluna especificada.
+    coluna e os dois menores valores dessa coluna. Ela vai associar os valores aos 
+    nomes dos países que cada um representa e especificar uma cor para cada país.
     '''
     maiores_valores = dataframe[nome_coluna].nlargest(2)
-    maiores_paises = dataframe[nome_coluna_pais].iloc[maiores_valores.index]
+    maiores_paises = dataframe[nome_coluna_pais].iloc[maiores_valores.index].tolist()
 
     menores_valores = dataframe[nome_coluna].nsmallest(2)
-    menores_paises = dataframe[nome_coluna_pais].iloc[menores_valores.index]
+    menores_paises = dataframe[nome_coluna_pais].iloc[menores_valores.index].tolist()
+
+    cores = {
+        'primeiro_maior': 'darkred',
+        'segundo_maior': 'red',
+        'primeiro_menor': 'darkblue',
+        'segundo_menor': 'lightblue'
+    }
+
+    mapeamento = {
+        maiores_paises[0]: 'primeiro_maior',
+        maiores_paises[1]: 'segundo_maior',
+        menores_paises[0]: 'primeiro_menor',
+        menores_paises[1]: 'segundo_menor'
+    }
 
     dicionario = {}
 
-    for valor, pais in zip(maiores_valores, maiores_paises):
-        dicionario[pais] = valor
-
-    for valor, pais in zip(menores_valores, menores_paises):
-        dicionario[pais] = valor
+    for pais in maiores_paises + menores_paises:
+        dicionario[pais] = cores[mapeamento[pais]]
 
     return dicionario
-
-data = {
-    'país': ['Brasil', 'Estados Unidos', 'China', 'Índia', 'Rússia'],
-    'valor': [211, 328, 1439, 1393, 144]
-}
-df = pd.DataFrame(data)
-
-data = funcao_maximo_minimo(df, 'valor', 'país')
-
-print(data)
