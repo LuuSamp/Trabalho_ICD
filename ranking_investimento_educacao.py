823802c3d9a95112c62d132cb01f2e1ba487a34f
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-26 15:29:14 -0300

    Adicionei o objeto base do gráfico

--- ranking_investimento_educacao.py
+++ ranking_investimento_educacao.py
@@ -6,3 +6,11 @@ import random
 # É adicionado uma lista com países e valores aleatórios relacionados a cada um.
 paises = ["Argentina", "Alemanha", "China", "Brasil", "EUA", "Japão", "Rússia"]
 valores = random.sample(range(0, 100), len(paises))
+
+# É adicionado o objeto base do gráfico
+investimento_educacao = figure(x_range=paises, height=350, title="Investimento em Educação por Países", toolbar_location=None, tools="")
+
+# É adicionado o gráfico de barras 
+investimento_educacao.vbar(x=paises, top=valores, width=0.9)
+
+show(investimento_educacao)
