2b439a0271fe60728041cb5278b1e234f359016c
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-26 19:09:21 -0300

    Fiz importações e conversões de dados

--- grafico_bolhas.py
+++ grafico_bolhas.py
@@ -1,1 +1,15 @@
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.io import output_file
+import pandas as pd
+from traducao_g20 import filtro_paises_do_g20
+from reorganizador import reorganiza, traduz_milhares
+
+
+df_populacao = reorganiza("dados/pop.csv")
+df_imc_homens = reorganiza("dados/body_mass_index_bmi_men_kgperm2.csv")
+df_imc_mulheres = ("dados/body_mass_index_bmi_women_kgperm2.csv")
+df_calorias = ("dados/food_supply_kilocalories_per_person_and_day.csv")
+
+
 
