2884a8ce3186f3c699e32cd417ef0c36283fcbbf
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-26 19:42:16 -0300

    Filtrei os países do G20

--- grafico_bolhas.py
+++ grafico_bolhas.py
@@ -11,6 +11,7 @@ df_imc_homens = reorganiza(datapath = "dados/body_mass_index_bmi_men_kgperm2.csv
 df_imc_mulheres = reorganiza(datapath = "dados/body_mass_index_bmi_women_kgperm2.csv", column_name = "IMC das Mulheres", first_year = 1990, last_year = 2008, csv = False)
 df_calorias = reorganiza(datapath = "dados/food_supply_kilocalories_per_person_and_day.csv", column_name = "Média de Calorias", first_year = 1990, last_year = 2008, csv = False)
 
+# Criando o DataFrame Final
 df_final = pd.DataFrame()
 df_final["country"] = df_populacao["country"]
 df_final["year"] = df_populacao["year"]
@@ -20,5 +21,7 @@ df_final["IMC das Mulheres"] = df_imc_mulheres["IMC das Mulheres"]
 df_final["Média de Calorias"] = df_calorias["Média de Calorias"]
 df_final['IMC Médio'] = (df_final['IMC dos Homens'] + df_final['IMC das Mulheres']) / 2
 
+# Filtro de Países do G20
+df_final = filtro_paises_do_g20(df_final)
 
 print(df_final)
