ad661f5177c7f9bce40fb2d81b08645ec7b99689
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-26 14:51:50 -0300

    Comentário sobre o objetivo da função.

---
+++ funcao_maximo_minimo.py
@@ -0,0 +1,6 @@
+def funcao_maximo_minimo (dataframe, nome_coluna):
+    '''
+    Essa função irá receber um Data Frame juntamente coma sua coluna que deve ser 
+    analisada. A função tem o objetivo de analisar os dois maiores valores dessa 
+    coluna e os dois menores valores dessa coluna.
+    '''
