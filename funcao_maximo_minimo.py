51e0abc1864484be7ba50c48c7895ba9a28f5be1
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-26 15:30:17 -0300

    Criando listas na função que recebem os dois maiores valores e os dois menores valores de uma coluna, através dos métodos "nlargest" e "nsmallest".

--- funcao_maximo_minimo.py
+++ funcao_maximo_minimo.py
@@ -1,6 +1,13 @@
+import pandas as pd
+
 def funcao_maximo_minimo (dataframe, nome_coluna):
     '''
     Essa função irá receber um Data Frame juntamente coma sua coluna que deve ser 
     analisada. A função tem o objetivo de analisar os dois maiores valores dessa 
     coluna e os dois menores valores dessa coluna.
     '''
+    maiores_valores = dataframe[nome_coluna].nlargest(2).tolist()
+    menores_valores = dataframe[nome_coluna].nsmallest(2).tolist()
+    return maiores_valores, menores_valores
+
+
