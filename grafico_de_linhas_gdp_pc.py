50e81e445c57eaa8ac83ea3d7bb00df5a7b34ca3
Matheus <matheuscarvalho210404@outlook.com>
2023-06-27 19:54:29 -0300

    CONFIGURANDO O GRÁFICO DE LINHAS

--- grafico_de_linhas_gdp_pc.py
+++ grafico_de_linhas_gdp_pc.py
@@ -2,11 +2,12 @@
 from bokeh.plotting import figure 
 from bokeh.io import output_file, save, show
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
-from reorganizador import reorganiza
+from reorganizador import reorganiza, traduz_milhares
 from traducao_g20 import filtro_paises_do_g20
-import math
+from variaveis_globais import *
 
-def grafico_de_linhas_gdp(datapath, titulo):
+
+def grafico_de_linhas_gdp(datapath):
 
     '''
     a função tem como objetivo receber o path dos arquivos e o 
@@ -14,24 +15,27 @@ def grafico_de_linhas_gdp(datapath, titulo):
     '''
 
     #TRATAMENTO DA BASE DE DADOS
-    dataframe = reorganiza(datapath, "indice_analisado", 1910, 2010) #vai filtrar a base de dados para os anos de 1990 e 2010
-    dataframe = filtro_paises_do_g20(dataframe, "indice_analisado") #vai filtrar a base de dados apenas para países do g20 e UE
+    dataframe = reorganiza(datapath, "PIB_PC", 1910, 2010) #vai filtrar a base de dados para os anos de 1990 e 2010
+    dataframe["PIB_PC"] = dataframe["PIB_PC"].apply(traduz_milhares).astype(float)
+    dataframe = filtro_paises_do_g20(dataframe, agrupamento="year")    
+
     source = ColumnDataSource(dataframe) #vai transformar o dataframe para o formato CDS
 
     #CONFECÇÃO DO GRÁFICO
 
     #configuração do nome do arquivo
-    output_file(f"{titulo}.html")
+    output_file("grafico_de_linhas_pib_pc.html")
 
     #criação do objeto figure
-    line_plot = figure(title=f"{titulo}", width = 1350, height = 720)
+    line_plot = figure(title="PIB Per Capita G20 (1910-2010)", width = 1080, height = 720, x_range = (1910, 2010), 
+                       y_range = (0, 60000))
 
     #adição da ferramenta hover
-    hover = HoverTool(tooltips=[('País', '@country'), ('Ano', '@year'), ('PIB Per Capita (Dólar)', '@indice_analisado{$0,0}')])
+    hover = HoverTool(tooltips=[('País', '@country'), ('Ano', '@year'), ('PIB Per Capita (Dólar)', '@PIB_PC{$0,0}')])
     line_plot.add_tools(hover)
 
     #dicionário de países de destaque e suas cores
-    paises_destacaveis = {"Brazil":"blue","Argentina":"royalblue","France":"skyblue","Germany":"coral","Canada":"red","Japan":"indianred"}
+    paises_destacaveis = DICT_CORES
 
     #criação das várias linhas e suas respectivas formatações
     for country in dataframe["country"].unique():
@@ -39,41 +43,36 @@ def grafico_de_linhas_gdp(datapath, titulo):
 
         #PAÍS DESTACADOS
         if country in paises_destacaveis.keys():
-            line_plot.line(x="year", y="indice_analisado", source=country_data, color=paises_destacaveis[country], line_width=3, line_alpha=0.8)
+            line_plot.line(x="year", y="PIB_PC", source=country_data, color=paises_destacaveis[country], line_width=3, line_alpha=0.8)
         
         #OUTROS PAÍSES
         else:
-            line_plot.line(x="year", y="indice_analisado", source=country_data, color="gray", line_width=2, line_alpha=0.25)
+            line_plot.line(x="year", y="PIB_PC", source=country_data, color=CORES_COMUNS, line_width=2, line_alpha=0.25)
 
     #CONFIGURAÇÕES ESTÉTICAS DOS EIXOS
-    line_plot.xaxis[0].ticker.desired_num_ticks = 20
-    line_plot.xaxis[0].ticker.num_minor_ticks = 0
-    line_plot.yaxis[0].ticker.desired_num_ticks = 10
-    line_plot.yaxis[0].ticker.num_minor_ticks = 0
+    line_plot.xaxis[0].ticker.desired_num_ticks = NUM_MAJOR_TICKS_X
+    line_plot.xaxis[0].ticker.num_minor_ticks = NUM_MINOR_TICKS
+    line_plot.yaxis[0].ticker.desired_num_ticks = NUM_MAJOR_TICKS_Y
+    line_plot.yaxis[0].ticker.num_minor_ticks = NUM_MINOR_TICKS
 
     line_plot.xaxis.axis_label = "Anos" 
     line_plot.yaxis.axis_label = "PIB Per Capita (Dólares)" 
 
-    line_plot.xaxis.axis_label_text_font = "Times"
-    line_plot.yaxis.axis_label_text_font = "Times"
+    line_plot.xaxis.axis_label_text_font = FONTE_TEXTO
+    line_plot.yaxis.axis_label_text_font = FONTE_TEXTO
 
-    line_plot.xaxis.axis_label_text_font_size = "14pt"
-    line_plot.yaxis.axis_label_text_font_size = "14pt"
+    line_plot.xaxis.axis_label_text_font_size = TAMANHO_TITULO_EIXOS
+    line_plot.yaxis.axis_label_text_font_size = TAMANHO_TITULO_EIXOS
 
-    line_plot.xgrid.grid_line_color = None
-    line_plot.ygrid.grid_line_color = None
+    line_plot.xgrid.grid_line_color = LINHAS_GRADE
+    line_plot.ygrid.grid_line_color = LINHAS_GRADE
 
     line_plot.yaxis.formatter = NumeralTickFormatter(format="$0,0")
 
     #CONFIGURAÇÃO DO TÍTULO
-    line_plot.title.text_font = "Times"
-    line_plot.title.text_font_size = "20pt"
+    line_plot.title.text_font = FONTE_TEXTO
+    line_plot.title.text_font_size = TAMANHO_TITULO
     line_plot.title.align = "center"
     line_plot.title.text_baseline = "middle"
     
-
-    
-
     show(line_plot)
-
-grafico_de_linhas_gdp("dados\gdp_pcap.csv", "PIB Per Capita G20 1910-2010")
--- variaveis_globais.py
+++ variaveis_globais.py
@@ -7,7 +7,7 @@ BACKGROUND = "#a1a1a1"
 FONTE_TEXTO = "Arial"
 TAMANHO_TITULO = "20pt"
 TAMANHO_TITULO_EIXOS = "15pt"
-NUM_MINOR_TICKS = 5
+NUM_MINOR_TICKS = 0
 NUM_MAJOR_TICKS_X = 20
 NUM_MAJOR_TICKS_Y = 15
 LINHAS_GRADE = None
@@ -16,7 +16,6 @@ dataframe_idh = reorganiza("dados\idh_total.csv", "IDH", 1990, 2010)
 dataframe_idh = filtro_paises_do_g20(dataframe_idh, agrupamento="country")
 del dataframe_idh["year"]
 DICT_CORES = funcao_maximo_minimo(dataframe_idh, "IDH", "country")
-print(DICT_CORES)
 
 CORES_COMUNS = "gray"
 ALPHA_DESTAQUES = 0.8
