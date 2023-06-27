158fca519c3b48fb5cc0c046531d7e29f4d201a5
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-27 17:43:04 -0300

    Organizando código.

--- plot_mapa_IDH.py
+++ plot_mapa_IDH.py
@@ -14,6 +14,8 @@ df_IDH = reorganiza("dados\hdi_human_development_index.csv", "IDH", 1990, 2010)
 # Utilizando a função  "filtro_paises_do_g20" para filtrar apenas os países do g20:
 df_IDH_g20 = filtro_paises_do_g20(df_IDH, agrupamento="country")
 df_IDH_g20 = df_IDH_g20.sort_values("IDH", ascending=False)
+
+# Deletando a coluna "year":
 del df_IDH_g20["year"]
 
 # Carregando os dados dos países usando geopandas:
@@ -22,11 +24,11 @@ world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 # Criando um objeto GeoJSONDataSource:
 dados_geograficos = GeoJSONDataSource(geojson=world.to_json())
 
-# Configurar a figura e adicionar o gráfico
+# Configurando a figura e adicionando o gráfico
 mapa_IDH = figure(title="Mapa Mundial")
 mapa_IDH.patches('xs', 'ys', fill_alpha=0.7, line_color='black', line_width=0.5, source=dados_geograficos)
 
-# Exibir o mapa
+# Exibindo o mapa
 output_file("mapa_mundial.html")
 show(mapa_IDH)
 
