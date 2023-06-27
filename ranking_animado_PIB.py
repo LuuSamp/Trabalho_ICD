5abe843f7dea8bb248afd690e500af6ba57dc7d0
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-06-27 18:10:59 -0300

    Novo filtro_g20 aplicado, altura das barras mudada

--- ranking_animado_PIB.py
+++ ranking_animado_PIB.py
@@ -6,10 +6,11 @@ from bokeh.io import curdoc
 import pandas as pd
 
 # Dataframe a ser usado
-dataframe = filtro_paises_do_g20(pd.read_csv("dados/total_gdp_ppp_inflation_adjusted.csv"))
+dataframe = pd.read_csv("dados/total_gdp_ppp_inflation_adjusted.csv")
 for year in range(1800, 2014):
     dataframe[str(year)] = dataframe[str(year)].apply(traduz_milhares)
 print(dataframe)
+dataframe = filtro_paises_do_g20(dataframe, True, "country")
 
 # Dados
 year = 1990
@@ -20,7 +21,7 @@ data_source = ColumnDataSource(raw_data)
 
 # O gráfico
 plot = figure(width=700, height=500, title="PIB dos países do G20", y_range=sorted_dataframe["country"])
-bars = plot.hbar(y = "country", right = "GDP", source = data_source)
+bars = plot.hbar(y = "country", right = "GDP", height = 0.9, source = data_source)
 
 # Atualização do gráfico
 def update_chart():
