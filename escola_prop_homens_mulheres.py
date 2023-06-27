30a054ef84b69145ade5ae8601cc255b0d95e640
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-06-27 16:34:51 -0300

    Gráfico, slider e button feitos

---
+++ escola_prop_homens_mulheres.py
@@ -0,0 +1,73 @@
+from bokeh.plotting import figure, column
+from bokeh.models import Slider, ColumnDataSource, Button
+from reorganizador import *
+from traducao_g20 import filtro_paises_do_g20
+from bokeh.io import curdoc
+import pandas as pd
+
+# Dataframe a ser usado
+dataframe_homens = filtro_paises_do_g20(pd.read_csv("dados/anos_homens_na_escola.csv"), True, "country")
+dataframe_mulheres = filtro_paises_do_g20(pd.read_csv("dados/anos_mulheres_na_escola.csv"), True, "country")
+dataframe_total = dataframe_homens.copy()
+print(dataframe_total)
+for year in range(1970, 2016):
+    dataframe_total[str(year)] = dataframe_homens[str(year)] + dataframe_mulheres[str(year)]
+print(dataframe_total)
+
+# Dados
+year = 1970
+#sorted_dataframe = dataframe.sort_values(by=[f"{year}"])
+raw_data = {"country": list(dataframe_total["country"]), 
+            "Homens": list(dataframe_homens[f"{year}"]/dataframe_total[f"{year}"]),
+            "Mulheres": list(dataframe_mulheres[f"{year}"]/dataframe_total[f"{year}"])}
+data_source = ColumnDataSource(raw_data)
+print(raw_data)
+
+# O gráfico
+plot = figure(width=700, height=500, title="Proporção nos anos escolares de homens e mulheres", y_range=dataframe_total["country"])
+bars = plot.hbar_stack(["Homens", "Mulheres"], y = "country", color = ["Blue", "Red"], source = data_source)
+
+# Atualização do gráfico
+def update_chart():
+    global year
+    year += 1
+    if year > 2015:
+        year = 1970
+    slider.value = year
+
+# O botão
+button = Button(label = "Play")
+
+callback = None
+def button_action():
+    '''
+    Função que é executada quando o botão é apertado. Inicia ou para as atualizações periódicas na tabela
+    '''
+    global callback
+    if button.label == "Play":
+        callback = curdoc().add_periodic_callback(update_chart, 100)
+        button.label = "Pause"
+    elif button.label == "Pause":
+        curdoc().remove_periodic_callback(callback)
+        button.label = "Play"
+
+button.on_click(button_action)
+
+# O Slider
+slider = Slider(start = 1970, end = 2015, value = 1970, step=1, title="Year")
+
+def slider_action(attr, old, new):
+    '''
+    Função que é executada ao mover o slider. Muda o ano para aquele do slider.
+    '''
+    global year
+    year = slider.value
+    raw_data = {"country": list(dataframe_total["country"]), 
+            "Homens": list(dataframe_homens[f"{year}"]/dataframe_total[f"{year}"]),
+            "Mulheres": list(dataframe_mulheres[f"{year}"]/dataframe_total[f"{year}"])}
+    bars[0].data_source.data = raw_data
+
+slider.on_change("value", slider_action)
+
+# A GUI
+curdoc().add_root(column(button, slider, plot))
