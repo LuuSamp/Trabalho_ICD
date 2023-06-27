eb6fc6719056f7685075323c9fc60e163886307f
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-27 11:52:35 -0300

    Adicionando comentários ao processo de criação.

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,3 +1,13 @@
+'''
+Esse código tem o objetivo de criar um Scatter Plot (Gráfico de Dispersão) que 
+compara dois assuntos bem importantes na área da saúde, a Vacinação de crianças 
+e a Mortalidade Infantil. Dessa forma, será possível verificar a quantidade de 
+crianças que morrem por não serem vacinadas.
+'''
+
+'''
+Bibliotecas e módulos necessários:
+'''
 from reorganizador import reorganiza
 import pandas as pd
 from bokeh.layouts import gridplot
@@ -5,14 +15,31 @@ from bokeh.models.annotations import Span, BoxAnnotation
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import Title  
 
+'''
+Criação de Data Frames "tratados" a partir da utilização da função "reorganiza": 
+'''
 df_vacina = reorganiza("dados/dtp3_immunized_percent_of_one_year_olds.csv", "Porcentagem de Vacinação", 1990, 2010)
 df_mortes = reorganiza("dados/child_mortality_0_5_year_olds_dying_per_1000_born.csv", "Mortes a cada 1000 nascimentos", 1990, 2010)
 df_vacina["Mortes a cada 1000 nascimentos"] = df_mortes["Mortes a cada 1000 nascimentos"]
 
+'''
+Criação do Scatter Plot:
+'''
+
+'''
+1. Definindo o arquivo de saída para o gráfico:
+'''
 output_file("scatter_plot.html")
 
+'''
+2. Criando uma variável para o título que o centraliza, aumenta seu tamanho e 
+muda sua cor:
+'''
 titulo = Title(text="Vacinação e Mortalidade Infantil", align="center", text_font_size = "20pt", text_color = "darkblue")
 
+'''
+3. 
+'''
 scatter_plot = figure(title=titulo, x_axis_label="Mortes a cada 1000 nascimentos", y_axis_label="Porcentagem de crianças vacinadas")
 scatter_plot.scatter(df_vacina["Mortes a cada 1000 nascimentos"], df_vacina["Porcentagem de Vacinação"], size=5, alpha = 0.5, color="red")
 
