f90e7473f4905d54fc47b89297c7f3f9555c8ede
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-27 11:17:24 -0300

    Importação de um módulo para centralizar o título.

--- scatter_plot.py
+++ scatter_plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from bokeh.layouts import gridplot
 from bokeh.models.annotations import Span, BoxAnnotation
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import Title
+from bokeh.models import Title  
 
 df_vacina = reorganiza("dados/dtp3_immunized_percent_of_one_year_olds.csv", "Porcentagem de Vacinação", 1990, 2010)
 df_mortes = reorganiza("dados/child_mortality_0_5_year_olds_dying_per_1000_born.csv", "Mortes a cada 1000 nascimentos", 1990, 2010)
