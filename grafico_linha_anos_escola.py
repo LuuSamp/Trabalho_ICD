d480a1344f5b0699dc65995f10c5262b323168fb
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-27 14:39:46 -0300

    Realizei importações

--- grafico_linha_anos_escola.py
+++ grafico_linha_anos_escola.py
@@ -1,1 +1,6 @@
+from bokeh.plotting import figure 
+from bokeh.io import output_file, save, show
+from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
+from reorganizador import reorganiza
+from traducao_g20 import filtro_paises_do_g20
 
