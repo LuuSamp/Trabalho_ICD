452b4acac28b854088e6cb77eb96257b7080c813
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-27 14:40:59 -0300

    Arrumando o código e fazendo novos comentários.

--- scatter_plot.py
+++ scatter_plot.py
@@ -21,12 +21,18 @@ Criação de Data Frames "tratados" a partir da utilização da função "reorga
 '''
 df_vacina = reorganiza("dados/dtp3_immunized_percent_of_one_year_olds.csv", "Porcentagem de Vacinação", 1990, 2010)
 df_mortes = reorganiza("dados/child_mortality_0_5_year_olds_dying_per_1000_born.csv", "Mortes a cada 1000 nascimentos", 1990, 2010)
-df_vacina["Mortes a cada 1000 nascimentos"] = df_mortes["Mortes a cada 1000 nascimentos"]
 
 '''
 Utilizando a função  "filtro_paises_do_g20" para filtrar apenas os países do g20:
 '''
-df_pronto = filtro_paises_do_g20(df_vacina)
+df_vacina_g20 = filtro_paises_do_g20(df_vacina, "Porcentagem de Vacinação")
+df_mortes_g20 = filtro_paises_do_g20(df_mortes, "Mortes a cada 1000 nascimentos")
+
+'''
+Adicionando a coluna "Mortes a cada 1000 nascimentos" do DataFrame "df_mortes_g20"
+para o "df_vacina_g20":
+'''
+df_vacina_g20["Mortes a cada 1000 nascimentos"] = df_mortes_g20["Mortes a cada 1000 nascimentos"]
 
 '''
 Criação do Scatter Plot:
@@ -49,7 +55,7 @@ um alpha e uma cor para os pontos do gráfico:
 '''
 scatter_plot = figure(title=titulo, x_axis_label="Porcentagem de crianças vacinadas", y_axis_label="Mortes a cada 1000 nascimentos")
 #OBS: Não há muita diferença do scatter para o circle: 
-scatter_plot.scatter(df_vacina["Porcentagem de Vacinação"], df_vacina["Mortes a cada 1000 nascimentos"], size=5, alpha = 0.5, color="red")
+scatter_plot.scatter(df_vacina_g20["Porcentagem de Vacinação"], df_vacina_g20["Mortes a cada 1000 nascimentos"], size=5, alpha = 0.5, color="red")
 
 '''
 4. Aumentando o tamanho do nome do eixo X e adicionando uma cor a ele:
