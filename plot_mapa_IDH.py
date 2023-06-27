ae36945a0e02a6d22f64eabadb8e6cfd593d22e5
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-27 17:25:39 -0300

    Alterando a função g20 e ordenando a coluna IDH.

--- plot_mapa_IDH.py
+++ plot_mapa_IDH.py
@@ -5,18 +5,21 @@ from bokeh.models import GeoJSONDataSource
 import geopandas as gpd
 from reorganizador import reorganiza
 from traducao_g20 import filtro_paises_do_g20
+from funcao_maximo_minimo import funcao_maximo_minimo
 import pandas as pd
 
 # Criação de Data Frames "tratados" a partir da utilização da função "reorganiza":
 df_IDH = reorganiza("dados\hdi_human_development_index.csv", "IDH", 1990, 2010)
 
 # Utilizando a função  "filtro_paises_do_g20" para filtrar apenas os países do g20:
-df_IDH_g20 = filtro_paises_do_g20(df_IDH, "IDH")
+df_IDH_g20 = filtro_paises_do_g20(df_IDH, agrupamento="country")
+df_IDH_g20 = df_IDH_g20.sort_values("IDH", ascending=False)
+del df_IDH_g20["year"]
 
-# Carregar os dados usando geopandas
+# Carregando os dados dos países usando geopandas:
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-# Criar um objeto GeoJSONDataSource
+# Criando um objeto GeoJSONDataSource:
 dados_geograficos = GeoJSONDataSource(geojson=world.to_json())
 
 # Configurar a figura e adicionar o gráfico
@@ -26,3 +29,5 @@ mapa_IDH.patches('xs', 'ys', fill_alpha=0.7, line_color='black', line_width=0.5,
 # Exibir o mapa
 output_file("mapa_mundial.html")
 show(mapa_IDH)
+
+print(df_IDH_g20)
