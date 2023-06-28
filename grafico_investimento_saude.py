437274f332ea79c3f0ffffe19b9d92791214b59d
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-28 17:41:00 -0300

    Criando gráfico.

--- grafico_investimento_saude.py
+++ grafico_investimento_saude.py
@@ -2,6 +2,8 @@ from reorganizador import reorganiza
 from traducao_g20 import filtro_paises_do_g20
 from variaveis_globais import *
 from cores import lista_cores, lista_alpha
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, show
 
 # Criando um Data Frame "tratado" a partir da utilização da função "reorganiza": 
 df_investimento_saude = reorganiza("dados/government_health_spending_of_total_gov_spending_percent.csv", "Investimento em Saúde", 1995, 2010)
@@ -20,5 +22,16 @@ lista_de_preenchimento = []
 
 lista_de_preenchimento.append(lista_cores(paises))
 lista_de_cores.append(lista_alpha(paises))
-print(type(paises))
+
+df_investimento_saude_g20["Cor"] = lista_de_cores
+df_investimento_saude_g20["Preenchimento"] = lista_de_preenchimento
+
+# Criando um ColumnDataSource:
+source = ColumnDataSource(df_investimento_saude_g20)
+
+# Base do Gráfico:
+ranking_investimento_saude_g20 = figure(x_range=df_investimento_saude_g20["country"], y_range=(0,20),height=ALTURA, width=LARGURA, title="Emissão de Carbono por Habitante", tools="")
+
+# Criação do Gráfico de Barras
+ranking_investimento_saude_g20.vbar(x="country", top="Investimento em Saúde", source=source, width=0.9, color="Cor", alpha="Preenchimento", line_color=COR_DA_LINHA, line_width=ESPESSURA_DA_LINHA, line_alpha=ALPHA_DA_LINHA)
 
