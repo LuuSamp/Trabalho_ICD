5808438252c54da470adb1b9c640c09bc0c75601
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-28 16:58:01 -0300

    Arrumando caminho do DataFrame.

--- grafico_investimento_saude.py
+++ grafico_investimento_saude.py
@@ -2,7 +2,9 @@ from reorganizador import reorganiza
 from traducao_g20 import filtro_paises_do_g20
 
 # Criando um Data Frame "tratado" a partir da utilização da função "reorganiza": 
-df_investimento_saude = reorganiza("dados/dtp3_immunized_percent_of_one_year_olds.csv", "Porcentagem de Vacinação", 1990, 2010)
+df_investimento_saude = reorganiza("dados/government_health_spending_of_total_gov_spending_percent.csv", "Investimento em Saúde", 1995, 2010)
 
 # Utilizando a função  "filtro_paises_do_g20" para filtrar apenas os países do g20:
-df_investimento_saude_g20 = filtro_paises_do_g20(df_investimento_saude, "Porcentagem de Vacinação")
+df_investimento_saude_g20 = filtro_paises_do_g20(df_investimento_saude)
+
+print(df_investimento_saude_g20)
