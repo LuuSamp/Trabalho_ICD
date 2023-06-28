a1c12605534891ed49491d86a35b613c915b281a
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-28 17:30:31 -0300

    Criando listas de cor e preenchimento.

--- grafico_investimento_saude.py
+++ grafico_investimento_saude.py
@@ -1,5 +1,7 @@
 from reorganizador import reorganiza
 from traducao_g20 import filtro_paises_do_g20
+from variaveis_globais import *
+from cores import lista_cores, lista_alpha
 
 # Criando um Data Frame "tratado" a partir da utilização da função "reorganiza": 
 df_investimento_saude = reorganiza("dados/government_health_spending_of_total_gov_spending_percent.csv", "Investimento em Saúde", 1995, 2010)
@@ -10,4 +12,13 @@ df_investimento_saude_g20 = filtro_paises_do_g20(df_investimento_saude, agrupame
 # Ordenando os valores da coluna "Investimento em Saúde":
 df_investimento_saude_g20 = df_investimento_saude_g20.sort_values(by='Investimento em Saúde', ascending=False).reset_index(drop=True)
 
-print(df_investimento_saude_g20)
+# Criando listas de cor e preenchimento para os países:
+paises = df_investimento_saude_g20["country"].apply 
+
+lista_de_cores = []
+lista_de_preenchimento = []
+
+lista_de_preenchimento.append(lista_cores(paises))
+lista_de_cores.append(lista_alpha(paises))
+print(type(paises))
+
