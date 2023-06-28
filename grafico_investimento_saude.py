af7b903c71f9e5d85112cb798d687d0ef1d3e912
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-28 17:08:21 -0300

    Ordenando o DataFrame.

--- grafico_investimento_saude.py
+++ grafico_investimento_saude.py
@@ -5,6 +5,9 @@ from traducao_g20 import filtro_paises_do_g20
 df_investimento_saude = reorganiza("dados/government_health_spending_of_total_gov_spending_percent.csv", "Investimento em Saúde", 1995, 2010)
 
 # Utilizando a função  "filtro_paises_do_g20" para filtrar apenas os países do g20:
-df_investimento_saude_g20 = filtro_paises_do_g20(df_investimento_saude)
+df_investimento_saude_g20 = filtro_paises_do_g20(df_investimento_saude, agrupamento="country")
+
+# Ordenando os valores da coluna "Investimento em Saúde":
+df_investimento_saude_g20 = df_investimento_saude_g20.sort_values(by='Investimento em Saúde', ascending=False).reset_index(drop=True)
 
 print(df_investimento_saude_g20)
