4d0b9985da2d356522dd7962649a163937209e28
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-28 15:13:16 -0300

    Finalizando gráfico de ranking

--- ranking_investimento_educacao.py
+++ ranking_investimento_educacao.py
@@ -1,82 +1,80 @@
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.io import output_file
-import random
 import pandas as pd
 from traducao_g20 import filtro_paises_do_g20
 from reorganizador import reorganiza
 from variaveis_globais import *
 
-#dataframe = pd.read_csv("dados/investimento_educacao.csv")
-#dataframe = filtro_paises_do_g20(dataframe, True, "country")
-#print(dataframe)
-#dataframe.iloc[:,1:] = dataframe.iloc[:,1:].interpolate(method = "linear", axis = 1, limit_direction="both")
-#print(dataframe)
+def grafico_ranking_co2(datapath):
+    # Tratamento de dados
+    df_co2 = reorganiza(datapath, column_name = "CO2", first_year = 1990, last_year = 2018, csv = False)
+    df_co2 = filtro_paises_do_g20(df_co2, agrupamento="country")
+    del df_co2["year"]
+
+    # Reoordenamento de DataFrame
+    df_co2 = df_co2.sort_values(by='CO2', ascending=False).reset_index(drop=True)
+
+    # Criando coluna de lista de cor e preenchimento relacionada a cada país
+    dicionario_de_cores = DICT_CORES
+    lista_de_cores = []
+    lista_de_preenchimento = []
+
+    for cada_pais in df_co2["country"]:
+        if cada_pais in dicionario_de_cores.keys():
+            lista_de_cores.append(dicionario_de_cores[cada_pais])
+            lista_de_preenchimento.append(ALPHA_DESTAQUES)
+        else:
+            lista_de_cores.append(CORES_COMUNS)
+            lista_de_preenchimento.append(ALPHA_COMUNS)
+
+    df_co2["color"] = lista_de_cores
+    df_co2["preenchimento"] = lista_de_preenchimento
 
 
-df_escola = reorganiza(datapath = "dados/anos_homens_na_escola.csv", column_name = "Média de anos na Escola", first_year = 1970, last_year = 2015, csv = False)
-df_escola = filtro_paises_do_g20(df_escola, "Média de anos na Escola").reset_index()
+    # Criando um ColumnDataSource
+    source = ColumnDataSource(df_co2)
 
-df_anos_escola = pd.DataFrame()
-df_anos_escola["country"] = df_escola["country"]
-df_anos_escola["year"] = df_escola["year"]
-df_anos_escola["Média de anos na Escola"] = df_escola["Média de anos na Escola"]
+    # Base do Gráfico
+    ranking_co2 = figure(x_range=df_co2["country"], y_range=(0,20),height=ALTURA, width=LARGURA, title="Emissão de Carbono por Habitante", tools="")
 
-# É criada uma lista com as colunas que iremos fazer a média.
-colunas_trabalhadas = ["Média de anos na Escola"]
+    # Criação do Gráfico de Barras
+    ranking_co2.vbar(x="country", top="CO2", source=source, width=0.9, color="color", alpha="preenchimento", line_color=COR_DA_LINHA, line_width=ESPESSURA_DA_LINHA, line_alpha=ALPHA_DA_LINHA)
 
-# É criada uma tabela contendo a média dos anos
-df_media_por_anos = df_anos_escola.groupby("country")[colunas_trabalhadas].mean().reset_index()
+    # Implementação de Ferramenta Hover
+    hover = HoverTool(tooltips=[('País', '@country'), 
+                                ('Emissão por Pessoa', '@CO2 toneladas')])
+    ranking_co2.add_tools(hover)
 
-# É reordenado o DataFrame de acordo com os valores
-df_ordenado = df_media_por_anos.sort_values(by='Média de anos na Escola', ascending=False)
+    # Adição de elementos estéticos ao Ranking
+    ranking_co2.background_fill_color = BACKGROUND_FILL
 
-#CONFIGURANDO A COLUNA DE CORES E PREENCHIMENTO
-dicionario_de_cores = DICT_CORES
-lista_de_cores = []
-lista_de_preenchimento = []
+    ranking_co2.xaxis.major_label_orientation = 0.7
 
-for cada_pais in df_ordenado["country"]:
-    if cada_pais in dicionario_de_cores.keys():
-        lista_de_cores.append(dicionario_de_cores[cada_pais])
-        lista_de_preenchimento.append(ALPHA_DESTAQUES)
-    else:
-        lista_de_cores.append(CORES_COMUNS)
-        lista_de_preenchimento.append(ALPHA_COMUNS)
+    ranking_co2.yaxis[0].ticker.desired_num_ticks = NUM_MAJOR_TICKS_Y
+    ranking_co2.yaxis[0].ticker.num_minor_ticks = NUM_MINOR_TICKS
 
-df_ordenado["color"] = lista_de_cores
-df_ordenado["preenchimento"] = lista_de_preenchimento
+    ranking_co2.xaxis.axis_label = "Países" 
+    ranking_co2.yaxis.axis_label = "Média da Emissão de CO2 por Pessoa (Toneladas)" 
 
+    ranking_co2.xaxis.axis_label_text_font = FONTE_TEXTO
+    ranking_co2.yaxis.axis_label_text_font = FONTE_TEXTO
 
-# Criando um ColumnDataSource
-source = ColumnDataSource(df_ordenado)
+    ranking_co2.xaxis.axis_label_text_font_size = TAMANHO_TITULO_EIXOS
+    ranking_co2.yaxis.axis_label_text_font_size = TAMANHO_TITULO_EIXOS
 
-# É adicionado o objeto base do gráfico
-investimento_educacao = figure(x_range=df_ordenado["country"], height=650, width=1300, title="Média de anos na Escola", toolbar_location=None, tools="")
+    ranking_co2.xgrid.grid_line_color = LINHAS_GRADE
+    ranking_co2.ygrid.grid_line_color = LINHAS_GRADE
 
-# É adicionado o gráfico de barras 
-investimento_educacao.vbar(x="country", top="Média de anos na Escola", source=source, width=0.9, color="color", alpha="preenchimento")
+    ranking_co2.title.text_font = FONTE_TEXTO
+    ranking_co2.title.text_font_size =TAMANHO_TITULO
+    ranking_co2.title.align = ALINHAMENTO_TITULO
+    ranking_co2.title.text_baseline = BASELINE_TITULO
 
-# É alterado o background do gráfico
-investimento_educacao.xgrid.grid_line_color = None
+    ranking_co2.toolbar.logo = None 
+    ranking_co2.toolbar.autohide = True 
+    ranking_co2.toolbar_location = POSICAO_BARRA_FERRAMENTAS
 
-# É rotacionado os rótulos do eixo x
-investimento_educacao.xaxis.major_label_orientation = 45
-
-# É alterado o tamanho do título
-investimento_educacao.title.text_font_size = TAMANHO_TITULO
-
-# Título Centralizado
-investimento_educacao.title.align = "center"
-
-# Tamanho do nome dos países alterado
-investimento_educacao.below[0].major_label_text_font_size = "14px"
-
-# É retirado as barras de fundo do background
-investimento_educacao.xgrid.grid_line_color = None
-investimento_educacao.ygrid.grid_line_color = None
-
-# Tamanho dos valores do eixo y alterado 
-investimento_educacao.yaxis.major_label_text_font_size = "12pt"
-
-show(investimento_educacao)
+    output_file("../Ranking_CO2.html")
+    show(ranking_co2)
+grafico_ranking_co2("dados/co2.csv")
