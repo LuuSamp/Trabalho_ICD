893f4c926b65da1fe48b231a99d78ded0f5d7872
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-06-28 10:33:21 -0300

    Adicionei cores ao gráfico

--- ranking_investimento_educacao.py
+++ ranking_investimento_educacao.py
@@ -5,6 +5,7 @@ import random
 import pandas as pd
 from traducao_g20 import filtro_paises_do_g20
 from reorganizador import reorganiza
+from variaveis_globais import *
 
 #dataframe = pd.read_csv("dados/investimento_educacao.csv")
 #dataframe = filtro_paises_do_g20(dataframe, True, "country")
@@ -27,68 +28,30 @@ colunas_trabalhadas = ["Média de anos na Escola"]
 # É criada uma tabela contendo a média dos anos
 df_media_por_anos = df_anos_escola.groupby("country")[colunas_trabalhadas].mean().reset_index()
 
-# É adicionado uma lista com países e valores aleatórios relacionados a cada um.
-paises = [
-    'Argentina',
-    'Australia',
-    'Austria',
-    'Belgium',
-    'Brazil',
-    'Bulgaria',
-    'Canada',
-    'China',
-    'Croatia',
-    'Cyprus',
-    'Czech Republic',
-    'Denmark',
-    'Estonia',
-    'Finland',
-    'France',
-    'Germany',
-    'Greece',
-    'Hungary',
-    'India',
-    'Indonesia',
-    'Ireland',
-    'Italy',
-    'Japan',
-    'Latvia',
-    'Lithuania',
-    'Luxembourg',
-    'Malta',
-    'Mexico',
-    'Netherlands',
-    'Poland',
-    'Portugal',
-    'Romania',
-    'Russia',
-    'Saudi Arabia',
-    'Slovakia',
-    'Slovenia',
-    'South Africa',
-    'South Korea',
-    'Spain',
-    'Sweden',
-    'Turkey',
-    'United Kingdom',
-    'United States'
-]
-valores = random.sample(range(0, 100), len(paises))
-
-# É criado um DataFrame a partir de um dicionário
-dicionario_paises = dict(zip(paises, valores))
-
-# É criado um DataFrame do Pandas com o país e seu respectivo valor
-df = pd.DataFrame.from_dict(dicionario_paises, orient='index', columns=['Valor']).reset_index()
-
 # É reordenado o DataFrame de acordo com os valores
-df_ordenado = df.sort_values(by='Valor', ascending=False)
+df_ordenado = df_media_por_anos.sort_values(by='Média de anos na Escola', ascending=False)
+
+#CONFIGURANDO A COLUNA DE CORES E PREENCHIMENTO
+dicionario_de_cores = DICT_CORES
+lista_de_cores = []
+lista_de_preenchimento = []
+
+for cada_pais in df_ordenado["country"]:
+    if cada_pais in dicionario_de_cores.keys():
+        lista_de_cores.append(dicionario_de_cores[cada_pais])
+        lista_de_preenchimento.append(ALPHA_DESTAQUES)
+    else:
+        lista_de_cores.append(CORES_COMUNS)
+        lista_de_preenchimento.append(ALPHA_COMUNS)
+
+df_ordenado["color"] = lista_de_cores
+df_ordenado["preenchimento"] = lista_de_preenchimento
 
 # É adicionado o objeto base do gráfico
-investimento_educacao = figure(x_range=df_ordenado["index"], height=650, width=1300, title="Investimento em Educação por Países", toolbar_location=None, tools="")
+investimento_educacao = figure(x_range=df_ordenado["country"], height=650, width=1300, title="Média de anos na Escola", toolbar_location=None, tools="")
 
 # É adicionado o gráfico de barras 
-investimento_educacao.vbar(x=df_ordenado["index"], top=df_ordenado["Valor"], width=0.9, color="#AEB7BF")
+investimento_educacao.vbar(x=df_ordenado["country"], top=df_ordenado["Média de anos na Escola"], width=0.9, color="color", alpha="preenchimento")
 
 # É alterado o background do gráfico
 investimento_educacao.xgrid.grid_line_color = None
@@ -107,6 +70,5 @@ investimento_educacao.below[0].major_label_text_font_size = '14px'
 
 # Tamanho dos valores do eixo y alterado 
 investimento_educacao.yaxis.major_label_text_font_size = "12pt"
-
 
 show(investimento_educacao)
