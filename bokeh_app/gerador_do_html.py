690e752a34075145f22e560add972e7aa9e9b65e
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-06-30 18:54:26 -0300

    Funcionando

--- bokeh_app/gerador_do_html.py
+++ bokeh_app/gerador_do_html.py
@@ -1,59 +1,16 @@
-#import dict_graficos
-from jinja2 import Template
+import dict_graficos
 from bokeh.io import output_file, save, show
 from bokeh.plotting import column, figure
 from bokeh.models import Div
 
-#dicionario = dict_graficos.dicionario_de_graficos
-
-template = Template(
-    '''
-    <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>plot</title>
-    <style>
-    .wrapper {
-        width: 800px;
-        background-color: yellow;
-        margin: 0 auto;
-        }
-
-    .plotdiv {
-        margin: 0 auto;
-        }
-    </style>
-    </head>
-    <body>
-    <div class='wrapper'>
-        {{ div }}
-    </div>
-    </body>
-    </html>
-    '''
-)
-
-template = """
-{% block postamble %}
-<style>
-.bk-root .bk {
-    margin: 0 auto !important;
-}
-</style>
-{% endblock %}
-"""
+dicionario = dict_graficos.dicionario_de_graficos
 
 output_file("../index.html")
 
 fig = figure(width = 500, height = 500)
 div = Div(text="aaaA")
 
-show(column(fig, div), template) 
-
-'''
 coisa_pra_mostrar = tuple([column(*each_plot) for each_plot in dicionario.values()])
 print(coisa_pra_mostrar)
 
-show(column(*coisa_pra_mostrar))    
-'''
+show(*coisa_pra_mostrar)
