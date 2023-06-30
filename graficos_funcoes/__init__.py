163bbbcc84cee5a84c95a2e80154b9f56d2c7937
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-06-30 10:44:55 -0300

    Alteradas para funcionarem melhor

--- graficos_funcoes/__init__.py
+++ graficos_funcoes/__init__.py
@@ -6,9 +6,9 @@ from grafico_linha_anos_escola import linha_escola
 from ranking_co2o import grafico_ranking_co2
 
 
-dicionario_de_graficos = {"Expectativa de Vida": box_plot_life("dados\life_expectancy_male"),
-                          "IMC X Calorias": grafico_bolhas("dados\pop.csv", "dados\body_mass_index_bmi_men_kgperm2.csv", "dados\body_mass_index_bmi_women_kgperm2.csv","dados\food_supply_kilocalories_per_person_and_day.csv"),
-                          "PIB Médio": graf_barras_pib("dados\total_gdp_ppp_inflation_adjusted.csv"),
-                          "PIB Per Capita": grafico_de_linhas_gdp("dados\gdp_pcap.csv"),
-                          "Anos Na Escola": linha_escola("dados\anos_homens_na_escola.csv", "dados\anos_mulheres_na_escola.csv"),
-                          "Emissão de CO2": grafico_ranking_co2("dados\co2.csv")}
+dicionario_de_graficos = {"Expectativa de Vida": box_plot_life("dados/life_expectancy_male.csv"),
+                          "IMC X Calorias": grafico_bolhas("dados/pop.csv", "dados/body_mass_index_bmi_men_kgperm2.csv", "dados/body_mass_index_bmi_women_kgperm2.csv","dados/food_supply_kilocalories_per_person_and_day.csv"),
+                          "PIB Médio": graf_barras_pib("dados/total_gdp_ppp_inflation_adjusted.csv"),
+                          "PIB Per Capita": grafico_de_linhas_gdp("dados/gdp_pcap.csv"),
+                          "Anos Na Escola": linha_escola("dados/anos_homens_na_escola.csv", "dados/anos_mulheres_na_escola.csv"),
+                          "Emissão de CO2": grafico_ranking_co2("dados/co2.csv")}
--- graficos_funcoes/main.py
+++ graficos_funcoes/main.py
@@ -1,7 +1,7 @@
 from bokeh.plotting import figure, column, row
 from bokeh.models import Button, Select, Paragraph
 from bokeh.io import curdoc
-from graficos_funcoes import dicionario_de_graficos
+from __init__ import dicionario_de_graficos
 
 selected_plot = 0
 plot, paragraph = list(dicionario_de_graficos.values())[selected_plot]
