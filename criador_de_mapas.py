1466aff4e602c269efab9de3b3362c679e2c8254
samucaaaaaa <mucaspeedpvp@gmail.com>
2023-06-29 21:15:17 -0300

    Organizando código.

--- criador_de_mapas.py
+++ criador_de_mapas.py
@@ -3,14 +3,15 @@ from bokeh.models import GeoJSONDataSource
 import geopandas as gpd
 import pandas as pd
 from bokeh.palettes import Blues
-from bokeh.models import LinearColorMapper, ColorBar
+from bokeh.models import LinearColorMapper, ColorBar, Range1d
 from bokeh.models import HoverTool
 from converte_iso import converte_iso2, converte_iso3
+from variaveis_globais import *
 
 def cria_mapa(dataframe, main_column_name: str, palette = Blues[6]):
     '''
     Recebe um dataframe com uma coluna "country", o nome de uma coluna com valores e uma paleta de cores.
-    Retorna um objeto figure do bokeh que contém um gráfico com os países contidos destacados e os valores neles
+    Retorna um objeto figure do bokeh que contém um gráfico com os países contidos destacados e os valores neles.
     '''
     # Adicionando uma coluna iso3 no DataFrame:
     coluna_iso2 = dataframe["country"].apply(converte_iso2)
@@ -33,10 +34,10 @@ def cria_mapa(dataframe, main_column_name: str, palette = Blues[6]):
 
     # Fazendo cortes lineares na escala para para aplicar paleta:
     color_mapper = LinearColorMapper(
-        palette = palette, 
-        low = dataframe[main_column_name].min(), 
-        high = dataframe[main_column_name].max(), 
-        nan_color = '#d9d9d9')
+    palette = palette, 
+    low = dataframe[main_column_name].min(), 
+    high = dataframe[main_column_name].max(), 
+    nan_color = '#d9d9d9')
     
     # Criando barras de cores:
     color_bar = ColorBar(
@@ -50,9 +51,12 @@ def cria_mapa(dataframe, main_column_name: str, palette = Blues[6]):
 )
 
     # Configurando a figura e adicionando o gráfico:
-    plot = figure(title="Mapa Mundial", 
-                  width = 1080, 
-                  height = 720)
+    plot = figure(title="Índice de Gini do G20", 
+                  width = LARGURA, 
+                    height = ALTURA, 
+                    x_range = Range1d(-180, 180, bounds="auto"), 
+                    y_range = Range1d(-90, 90, bounds="auto"),
+                    tools="pan,reset,wheel_zoom,box_zoom")
     
     plot.patches('xs', 'ys', 
                  fill_alpha=0.7, 
@@ -68,6 +72,10 @@ def cria_mapa(dataframe, main_column_name: str, palette = Blues[6]):
     # Adicionando barra com grade de cor: 
     plot.add_layout(color_bar, 'below')
 
+    plot.toolbar.logo = None 
+    plot.toolbar.autohide = True 
+    plot.toolbar_location = POSICAO_BARRA_FERRAMENTAS
+
     # Adicionando hover:
     hover = HoverTool(
         tooltips = [('País','@name'),
@@ -75,4 +83,14 @@ def cria_mapa(dataframe, main_column_name: str, palette = Blues[6]):
         renderers = [paises_g20]
         )
     plot.add_tools(hover)
+
+    # Configurações estéticas:
+    plot.xaxis[0].ticker.desired_num_ticks = 20
+    plot.yaxis[0].ticker.desired_num_ticks = NUM_MAJOR_TICKS_Y
+    plot.yaxis[0].ticker.num_minor_ticks = NUM_MINOR_TICKS
+
+    plot.title.text_font = FONTE_TEXTO
+    plot.title.text_font_size =TAMANHO_TITULO
+    plot.title.align = ALINHAMENTO_TITULO
+    plot.title.text_baseline = BASELINE_TITULO
     return plot
